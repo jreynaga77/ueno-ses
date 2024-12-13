724cd75ddd98756b06b20743a9d27049cbaf7a30
jonathan-reynaga-ueno <jonathan.reynaga@itti.digital>
2024-12-13 20:51:53 -0300

    ss

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "ses_domain_identity_arn" {
-  value = aws_ses_domain_identity.domain.*.arn
+  value = aws_ses_domain_identity.domain[0].arn
 }
 
 output "template_name" {
-  value = aws_ses_template.template.*.template_name
+  value = aws_ses_email_template.template[0].template_name
 }
